14093b35e34311433206f80c5896b3995287b27b
C.Dincer <jemydev92@gmail.com>
2026-03-23 22:40:51 +0100

    Update
    Add Substraction
    Substraction Object Calculate 2 Number
    with integers x and y with the - operator
    
    Works!

--- src/v01/Calculate.java
+++ src/v01/Calculate.java
@@ -9,7 +9,12 @@ package v01;
 
 class Calculate {
     static void main(String[] args) {
+        /*Addition*/
        Addition myObj = new Addition(); // erstellt ein neues objekt
        System.out.println("Das Ergebnis ist: " + myObj.sum);   // gibt das objekt aus das zugewiesen wurde
+
+        /*Subtraction*/
+        Subtraction myObj2 = new Subtraction();
+        System.out.println("Das Ergebnis ist: " + myObj2.sum);
     }
 }
---
+++ src/v01/Subtraction.java
@@ -0,0 +1,8 @@
+/*Subtrahiert 2 Zahlen*/
+package v01;
+
+public class Subtraction {
+    int x  = 30;
+    int y = 10;
+    int sum = x - y;
+}
