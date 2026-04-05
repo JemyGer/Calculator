2ce18f00e0847e0e14e8a49df5c884dd8cffabad
C.Dincer <jemydev92@gmail.com>
2026-04-05 09:48:29 +0200

    Update
    Added Scene Builder
    ( for my JavaFX Application )
    it will be used for layout stuff
    
    . . . . under developing
    ( includes detailed notes as well, in the commits later)

---
+++ src/v02/CalculatorController.java
@@ -0,0 +1,13 @@
+/* packages */
+package v02;
+
+/* imports */
+import javafx.fxml.FXML;
+
+public class CalculatorController {
+
+    @FXML
+    private void handlePortfolioClick() {
+        System.out.println("Portfolio clicked");
+    }
+}
--- src/v02/CalculatorGUI.java
+++ src/v02/CalculatorGUI.java
@@ -3,17 +3,22 @@ package v02;
 
 /*imports*/
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
   public class CalculatorGUI extends Application{
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("c.dincer.javadeveloper");
-        Scene scene = new Scene(label, 500, 500);
-        primaryStage.setTitle("Calculator_V02");
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/entry.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("calculator_V02");
         primaryStage.setScene(scene);
         primaryStage.show();
 
